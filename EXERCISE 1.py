38bd825a60075829d4e10e916d0e41832ee9cfb2
Poornimj <poornimj@metropolia.fi>
2024-08-29 12:36:50 +0530

    CHECK

--- EXERCISE 1.py
+++ EXERCISE 1.py
@@ -1,9 +1,20 @@
-size_limit = 42
-zander_length = float(input("Enter the length of the zander in centimeters: "))
-
-if zander_length >= size_limit:
-    print("The zander meets the size limit, You can keep the fish.")
+Size_limit = 42
+Zander_length = float(input(" Enter the length of the Zander in centimeters: "))
+if Zander_length >= Size_limit:
+    print(" The Zander meets the size limit, You can keep the fish.")
 else:
-    difference = size_limit - zander_length
-    print(f"The zander is below the size limit by {difference:.1f} centimeters.")
+    Difference = Size_limit - Zander_length
+    print(f"The Zander is below the size limit by {Difference:.1f} centimeters.")
     print(" Kindly release the fish back into the lake.")
+
+
+
+
+
+
+
+
+
+
+
+
