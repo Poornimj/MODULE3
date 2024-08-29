9c734351a3fcae0ac814c76fffe7ffcc2be7090b
Poornimj <poornimj@metropolia.fi>
2024-08-29 13:05:31 +0530

    CHECK

--- EXERCISE 2.py
+++ EXERCISE 2.py
@@ -1,12 +1,12 @@
-cabin_class = input("Please enter the cabin class (LUX, A, B, C): ")
-
-if cabin_class == "LUX or lux":
-    print("LUX: Upper-deck cabin with a balcony.")
-elif cabin_class == "A":
+Cabin_Class = input(" Please enter the cabin class (LUX, A, B, C):")
+if Cabin_Class == " LUX ":
+    print("LUX: Upper-Deck cabin with a balcony.")
+elif Cabin_Class == "A":
     print("A: Above the car deck, equipped with a window.")
-elif cabin_class == "B":
+elif Cabin_Class == "B":
     print("B: Windowless cabin above the car deck.")
-elif cabin_class == "C":
-    print("C: Windowless cabin below the car deck.")
+elif Cabin_Class == "C":
+    print("C: Windowless Cabin below the car deck")
 else:
     print("Invalid cabin class.")
+
